7ee0867a8d0f90a552f20af83f620fc5b06767d8
SergeyKelme <SergeyKelme@yandex.ru>
2024-11-01 18:58:48 +0400

    added tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,5 +12,20 @@ class TestConfig(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'I am seeking an opportunity to join', response.data)
 
+    def test_get_about(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'- Programming Language: Python', response.data)
+
+    def test_get_contacts(self):
+        response = self.client.get('/contacts/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b' I am ready to provide additional information', response.data)
+
+    def test_get_thank(self):
+        response = self.client.get('/thank/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Thank you for your', response.data)
+
 if __name__ == '__main__':
     pytest.main()
